845388bef5ad2facff48cc1fc852d3733c8194f9
Tom Goodison <tomrgoodison@gmail.com>
2025-06-12 12:37:28 +1000

    Added User Input

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import os
+import os, sys
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 from google import genai
-client = genai.Client(api_key=api_key)
-model = 'gemini-2.0-flash-001'
 
-response = client.models.generate_content(model=model, contents = "Why is Boot.dev such a great place to learn backend development? " \
-"Use one paragraph maximum.")
+def main():
+    load_dotenv()
+    args = sys.argv[1:]
 
-print(response.text)
+    if not args:
+        print("no prompt provided")
+        sys.exit(1)
+    user_prompt = " ".join(args)
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=user_prompt,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
